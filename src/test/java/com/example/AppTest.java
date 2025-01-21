df4ed1400d8ecbd47dbc67a46337bafd81ea8631
rishihudar <95029689+rishihudar@users.noreply.github.com>
2025-01-21 18:09:55 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,12 @@
 package com.example;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
+    
     @Test
-    public void testMain() {
+    public void testMethod() {
         assertTrue(true);
     }
 }
